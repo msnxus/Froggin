42a6df7ae5777aba65ada0f928eac7b93ca1d30a
ayo-oguntola <aoguntola@gmail.com>
2023-12-11 14:06:35 -0500

    Added event listeners

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -26,6 +26,35 @@ class SeedScene extends Scene {
 
         // Populate GUI
         this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
+
+        window.addEventListener('keyup', function (event) {
+            // Ignore keypresses typed into a text box
+            if (event.target.tagName === 'INPUT') return;
+            console.log(event.key);
+            // if 'I' was released, download the image
+            if (event.key === ' ') {
+                flower.spin();
+            }
+
+            const keyMap = {
+                ArrowUp: 'ArrowUp',
+                ArrowDown: 'ArrowDown',
+                ArrowLeft: 'ArrowLeft',
+                ArrowRight: 'ArrowRight',
+            };
+
+            if (Object.keys(keyMap).find((v) => v == event.key)) {
+                if (event.key == keyMap.ArrowDown) {
+                    flower.position.x -= 1;
+                } else if (event.key == keyMap.ArrowUp) {
+                    flower.position.x += 1;
+                } else if (event.key == keyMap.ArrowLeft) {
+                    flower.position.z -= 1;
+                } else if (event.key == keyMap.ArrowRight) {
+                    flower.position.z += 1;
+                }
+            }
+        });
     }
 
     addToUpdateList(object) {
