ba2b484818892577768e2086a756e674c9949237
Ryan Hoffman <rh8490@princeton.edu>
2023-12-15 18:24:52 -0500

    adjusted params

--- src/params.js
+++ src/params.js
@@ -47,7 +47,7 @@ const SceneParams = {
     NUM_INITIAL_PADS: 2,
 
     // Furthest a pad can be
-    LILYPAD_MAX_JUMP_RADIUS: 25,
+    LILYPAD_MAX_JUMP_RADIUS: 20,
 };
 
 export default SceneParams;
