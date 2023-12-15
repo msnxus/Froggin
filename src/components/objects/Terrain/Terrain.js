ce19c0b31ca4312786593b0216307c510ac7ed68
Nicolas Nee <nn3965@princeton.edu>
2023-12-15 18:51:56 -0500

    cave terrrain

--- src/components/objects/Terrain/Terrain.js
+++ src/components/objects/Terrain/Terrain.js
@@ -1,9 +1,9 @@
 import { Group, Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import WATERFALL from './waterfall.glb';
 import GRASSPOND from './grassPond.glb';
 import WOODS from './woods.glb';
 import DESERT from './desert.glb';
+import CAVE from './cave.glb';
 
 import { WebGLAttributes } from 'three/src/renderers/webgl/WebGLAttributes';
 
@@ -17,23 +17,22 @@ class Terrain extends Group {
         const loader = new GLTFLoader();
         
         this.offset = frog.position.clone();
-        // WATERFALL -> Waterfall scene by Nikki Morin [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/dkbjOPcyB2l)
+        // CAVE -> Cave Scene | Draft 1 by Danni Bittman [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/2VmWkpO6OvK)
         // GRASSPOND -> Pond by Jarlan Perez [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/1XazZeNBWG_)
         // WOODS -> Nature by 3Donimus [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/0nsE2b8uXZy)
         // DESERT -> Desert by Poly by Google [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/a1HnTCHfE34)
-        const SCENES = [DESERT, WOODS, WATERFALL, GRASSPOND];
+        const SCENES = [CAVE, DESERT, WOODS, GRASSPOND];
         const randNum = Math.floor(Math.random() * SCENES.length);
         this.name = SCENES[randNum];
 
         loader.load(SCENES[randNum], (gltf) => {
-            let scale = 110;
+            let caveScale = 30;
+            let scale = 100;
             let grassScale = 255;
             let woodsScale = 50;
             let desertScale = 0.2;
             
 
-            gltf.scene.scale.set(scale, scale, scale);
-            gltf.scene.position.set(0, 0, 0).add(this.offset);
             gltf.scene.rotation.y = Math.PI;
             if(SCENES[randNum] == GRASSPOND) {
                 gltf.scene.scale.set(grassScale, scale, grassScale);
@@ -48,9 +47,9 @@ class Terrain extends Group {
                 gltf.scene.scale.set(desertScale, desertScale, desertScale);
                 gltf.scene.position.set(95, -25, 0).add(this.offset);
             }
-            if(SCENES[randNum] == WATERFALL) {
-                gltf.scene.scale.set(scale, scale, scale);
-                gltf.scene.position.set(95, -10, -10).add(this.offset);
+            if(SCENES[randNum] == CAVE) {
+                gltf.scene.scale.set(caveScale, caveScale, caveScale);
+                gltf.scene.position.set(100, -20, 20).add(this.offset);
                 gltf.scene.rotation.y = Math.PI / 2;
 
             }
